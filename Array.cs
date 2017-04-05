c9f1c7ae79efdcd3cca21c114bfa3108a3e2078a
Patryk K <patrykxk@users.noreply.github.com>
2017-04-05 13:46:15 +0200

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Array
+    {
+        static int N = 100;
+        private int[] tab = new int[N];
+
+        public int this[int i] { 
+            get {
+                if (i < tab.Length)
+                {
+                    return tab[i];
+                }
+                else {
+                    throw new System.IndexOutOfRangeException();
+                }
+            }
+            set {
+               
+                tab[i] = value;
+
+            }
+        }
+        public void add(int x){
+            tab[tab.Length-1] = x;
+        }
+         
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Array tab = new Array();
+
+            tab[0] = 11111;
+            Console.WriteLine(tab[0]);
+            tab[102] = 102;
+            
+            tab.add(3);
+            tab[202] = 202;
+
+
+            Console.WriteLine(tab[0] + " " + tab[102] + " " + tab[202]);
+        }
+    }
+}
